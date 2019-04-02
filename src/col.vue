1b29ac548f6a219446c191e7307e18bee61ba116
lovexwu <1062474646@qq.com>
2019-04-02 18:16:28 +0800

    重构 row 和 col

--- src/col.vue
+++ src/col.vue
@@ -1,9 +1,6 @@
 <template>
-    <div class="col" :class="[span && `col-${span}`,offset && `offset-${offset}`]"
-    :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
-        <div style="border: 1px solid green;">
-            <slot></slot>
-        </div>
+    <div class="col" :class="colClass" :style="colStyle">
+        <slot></slot>
     </div>
 </template>
 
@@ -24,11 +21,20 @@
                 gutter: 0
             }
         },
-        created(){
-            console.log('col created') //没有儿子
-        },
-        mounted() {
-            console.log('col mounted')
+        computed:{
+            colClass(){
+                let {span, offset} = this
+                return [
+                    span && `col-${span}`,
+                    offset && `offset-${offset}`
+                ]
+            },
+            colStyle(){
+                return {
+                    paddingLeft:  this.gutter/2 + 'px',
+                    paddingRight: this.gutter/2 + 'px'
+                }
+            }
         }
     }
 </script>
