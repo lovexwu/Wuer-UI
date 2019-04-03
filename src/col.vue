6a6ddb1d9997f86d936be6759e00bc46efc2f9c6
lovexwu <1062474646@qq.com>
2019-04-03 15:31:18 +0800

    稍微重构了一下

--- src/col.vue
+++ src/col.vue
@@ -36,17 +36,25 @@
                 gutter: 0,
             }
         },
+        methods:{
+            createClasses (obj,str = '') {
+                if(!obj) {return []}
+                let array = []
+                if(obj.span){ array.push(`col-${str}${obj.span}`)}
+                if(obj.offset){ array.push(`offset-${str}${obj.offset}`)}
+                return array
+            }
+        },
         computed:{
             colClass(){
                 let {span, offset, ipad, narrowPc, pc, widePc} = this
-                let phoneClass =[]
+                let createClasses = this.createClasses
                 return [
-                    span && `col-${span}`,
-                    offset && `offset-${offset}`,
-                    ...(ipad && [`col-ipad-${ipad.span}`]),
-                    ...(narrowPc && [`col-narrow-pc-${narrowPc.span}`]),
-                    ...(pc && [`col-pc-${pc.span}`]),
-                    ...(widePc && [`col-wide-pc-${widePc.span}`]),
+                    ...createClasses({span,offset}),
+                    ...createClasses(ipad, 'ipad-'),
+                    ...createClasses(narrowPc,'narrow-pc-'),
+                    ...createClasses(pc,'pc-'),
+                    ...createClasses(widePc,'wide-pc-'),
                 ]
             },
             colStyle(){
@@ -76,7 +84,7 @@
             }
         }
 
-        @media (min-width:577px) and (max-width:768px){
+        @media (min-width:577px) {
             $class-prefix: col-ipad-;
             @for $n from 1 through 24 {
                 &.#{$class-prefix}#{$n} {
@@ -92,7 +100,7 @@
             }
         }
 
-        @media (min-width:769px) and (max-width:992px){
+        @media (min-width:769px){
             $class-prefix: col-narrow-pc-;
             @for $n from 1 through 24 {
                 &.#{$class-prefix}#{$n} {
@@ -108,7 +116,7 @@
             }
         }
 
-        @media (min-width:993px) and (max-width:1200px){
+        @media (min-width:993px){
             $class-prefix: col-pc-;
             @for $n from 1 through 24 {
                 &.#{$class-prefix}#{$n} {
@@ -140,7 +148,6 @@
                 }
             }
         }
-
     }
 
 </style>
