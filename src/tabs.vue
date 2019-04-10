1223a3ef9e0125f7ab756fdace7e988e642a5d82
lovexwu <1062474646@qq.com>
2019-04-10 13:25:39 +0800

    添加部分 测试用例

--- src/tabs.vue
+++ src/tabs.vue
@@ -32,15 +32,27 @@
             }
         },
         mounted(){
+
+            if(this.$children.length === 0){
+                console && console.warn &&
+                console.warn('tabs 的 子组件应该是 tabs-head 和 tabs-body,但你没写子组件')
+            }
+
             this.$children.forEach((vm)=>{
+
                 if(vm.$options.name === 'GuluTabsHead'){
+
                     vm.$children.forEach((childVm)=>{
-                        if(childVm.$options.name === 'GuluTabsItem' && childVm.name === this.selected){
+                        if(childVm.$options.name === 'GuluTabsItem'
+                            && childVm.name === this.selected){
+
                             this.eventBus.$emit('update:selected',this.selected, childVm)
+
                         }
                     })
                 }
             })
+
         }
     }
 </script>
