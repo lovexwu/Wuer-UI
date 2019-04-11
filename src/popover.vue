fadc21983b7a738b45a0ba8a07d1cfac758474c7
lovexwu <1062474646@qq.com>
2019-04-11 16:43:42 +0800

    添加默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,7 @@
         <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
-        <span ref="triggerWrapper">
+        <span ref="triggerWrapper" style="display: inline-block;">
          <slot></slot>
         </span>
 
@@ -13,20 +13,25 @@
 <script>
     export default {
         name: "GuluPopover",
-        data (){
+        data: function () {
             return {visible: false}
         },
         methods:{
-            positionContent(){
+            positionContent: function () {
                 document.body.appendChild(this.$refs.contentWrapper)
-                let {width,height,top,left} = this.$refs.triggerWrapper.getBoundingClientRect()
-                this.$refs.contentWrapper.style.left = left + window.scrollX+'px'
-                this.$refs.contentWrapper.style.top = top + window.scrollY+'px'
+                let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+                this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+                this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
             },
             onClickDocument (event) {
                 if (this.$refs.popover &&
                     (this.$refs.popover === event.target || this.$refs.popover.contains(event.target)) ) {
-                    return;
+                    return
+                }
+                if (this.$refs.contentWrapper &&
+                    (this.$refs.contentWrapper === event.target || this.$refs.contentWrapper.contains(event.target))
+                ){
+                    return
                 }
                 this.close()
             },
@@ -56,6 +61,8 @@
 </script>
 
 <style lang="scss" scoped>
+    $border-color: #333;
+    $border-radius: 4px;
     .popover{
         display: inline-block;
         vertical-align: top;
@@ -63,8 +70,34 @@
     }
     .content-wrapper{
         position: absolute;
-        border: 1px solid red;
-        box-shadow: 0 0 3px rgba(0,0,0,0.5);
+        border: 1px solid $border-color;
+        border-radius: $border-radius;
+        //box-shadow: 0 0 3px rgba(0,0,0,0.5);
+        background: white;
+        filter: drop-shadow(0 1px 1px rgba(0,0,0,0.5));
         transform: translateY(-100%);
+        margin-top: -10px;
+        max-width: 20em;
+        padding: .5em 1em;
+        word-break: break-all;
+
+        &::before,&::after{
+            content: '';
+            display: block;
+            border: 10px solid transparent;
+            width: 0px;
+            height: 0px;
+            position: absolute;
+            left: 10px;
+        }
+        &::before{
+            border-top-color: black;
+            top: 100%;
+
+        }
+        &::after{
+            border-top-color: white;
+            top: calc(100% - 1px);
+        }
     }
 </style>
