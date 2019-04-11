5269631f3285e71f76d5394267e65ebeeda12fbb
lovexwu <1062474646@qq.com>
2019-04-11 15:42:00 +0800

    重构 tabs.vue

--- src/tabs.vue
+++ src/tabs.vue
@@ -31,28 +31,29 @@
                 eventBus: this.eventBus
             }
         },
+        methods:{
+           checkChildren(){
+               if(this.$children.length === 0){
+                   console && console.warn &&
+                   console.warn('tabs 的 子组件应该是 tabs-head 和 tabs-body,但你没写子组件')
+               }
+           },
+           selectTab(){
+               this.$children.forEach((vm)=>{
+                   if(vm.$options.name === 'GuluTabsHead'){
+                       vm.$children.forEach((childVm)=>{
+                           if(childVm.$options.name === 'GuluTabsItem'
+                               && childVm.name === this.selected){
+                               this.eventBus.$emit('update:selected',this.selected, childVm)
+                           }
+                       })
+                   }
+               })
+           }
+        },
         mounted(){
-
-            if(this.$children.length === 0){
-                console && console.warn &&
-                console.warn('tabs 的 子组件应该是 tabs-head 和 tabs-body,但你没写子组件')
-            }
-
-            this.$children.forEach((vm)=>{
-
-                if(vm.$options.name === 'GuluTabsHead'){
-
-                    vm.$children.forEach((childVm)=>{
-                        if(childVm.$options.name === 'GuluTabsItem'
-                            && childVm.name === this.selected){
-
-                            this.eventBus.$emit('update:selected',this.selected, childVm)
-
-                        }
-                    })
-                }
-            })
-
+            this.checkChildren()
+            this.selectTab()
         }
     }
 </script>
