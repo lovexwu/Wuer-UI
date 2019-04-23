73020208112e902cd208bf1621c7a3499a5df208
lovexwu <1062474646@qq.com>
2019-04-23 13:45:17 +0800

    更新 collapse 手风琴

--- docs/.vuepress/components/collapse-demo-1.vue
+++ docs/.vuepress/components/collapse-demo-1.vue
@@ -1,31 +1,13 @@
 <template>
-    <div style="padding-top: 16px;">
-        <h2>打开一个tab</h2>
-        <p>
-            <strong>预览</strong>
-        </p>
-
+    <div style="padding-top: 15px;">
         <g-collapse :selected.sync="selectedTab" single>
             <g-collapse-item title="标题1" name="1">内容1</g-collapse-item>
             <g-collapse-item title="标题2" name="2">内容2</g-collapse-item>
             <g-collapse-item title="标题3" name="3">内容3</g-collapse-item>
         </g-collapse>
         <p>选中的有：{{selectedTab}}</p>
-
-        <p>
-            <strong>代码</strong>
-        </p>
-        <pre><code>{{content}}</code></pre>
-
     </div>
 </template>
-
-<style  scoped>
-    *{
-        box-sizing: border-box;
-    }
-</style>
-
 
 <script>
 import Collapse from '../../../src/collapse'
@@ -38,14 +20,7 @@ export default {
     },
     data () {
         return {
-        selectedTab: ['1'],
-        content:`
-            <g-collapse :selected.sync="selectedTab" single>
-                <g-collapse-item title="标题1" name="1">内容1</g-collapse-item>
-                <g-collapse-item title="标题2" name="2">内容2</g-collapse-item>
-                <g-collapse-item title="标题3" name="3">内容3</g-collapse-item>
-            </g-collapse>
-        `.replace(/^ {12}/gm,'').trim()
+        selectedTab: ['1']
         }
     }
 }
