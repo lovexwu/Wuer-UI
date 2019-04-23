1ec9a5829dd1f9da70a447c42388a981978606cf
lovexwu <1062474646@qq.com>
2019-04-23 13:46:38 +0800

    更新 input 输入框

--- docs/.vuepress/components/input-demo-1.vue
+++ docs/.vuepress/components/input-demo-1.vue
@@ -1,45 +1,15 @@
 <template>
-    <div style="padding-top: 16px;">
-        <h2>简单用法</h2>
-        <p>
-        <strong>预览</strong>
-        </p>
+    <div style="padding-top: 15px;">
         <g-input value="张三"></g-input>
         <g-input value="李四" disabled></g-input>
         <g-input value="王五" readonly error="姓名不能少于两个字"></g-input>
-
-        <p>
-        <strong>代码</strong>
-        </p>
-        <pre><code>{{content}}</code></pre>
     </div>
 </template>
-
-<style scoped>
-    * {box-sizing: border-box;}
-    h2{margin: 50px 0 30px;}
-</style>
 
 <script>
     import GInput from '../../../src/input'
 
     export default {
-        components: {'g-input': GInput},
-        data () {
-            return {
-            selected: '1',
-            content: `
-                <g-input value="张三"></g-input>
-                <g-input value="李四" disabled></g-input>
-                <g-input value="王五" readonly></g-input>
-
-                <g-input v-model="message"></g-input>
-                <button @click="message+=1">
-                    +
-                </button>
-                {{message}}
-            `.replace(/^ {12}/gm, '').trim()
-            }
-        }
+        components: {'g-input': GInput}
       }
 </script>
--- docs/.vuepress/components/input-demo-2.vue
+++ docs/.vuepress/components/input-demo-2.vue
@@ -1,26 +1,12 @@
 <template>
-    <div style="padding-top: 16px;">
-        <h2>双向绑定</h2>
-        <p>
-        <strong>预览</strong>
-        </p>
-        <g-input v-model="message"></g-input>
-        <button @click="message+=1">
+    <div style="padding-top: 15px;">
+        <g-input v-model="value"></g-input>
+        <button @click="value+=1">
             +
         </button>
-        {{message}}
-        <p>
-        <strong>代码</strong>
-        </p>
-        <pre><code>{{content}}</code></pre>
+        {{value}}
     </div>
 </template>
-
-
-<style scoped>
-* {box-sizing: border-box;}
-h2{margin: 50px 0 30px;}
-</style>
 
 <script>
 import GInput from '../../../src/input'
@@ -29,14 +15,7 @@ export default {
     components: {'g-input': GInput},
     data () {
         return {
-        message: '1',
-        content: `
-            <g-input v-model="message"></g-input>
-            <button @click="message+=1">
-                +
-            </button>
-            {{message}}
-        `.replace(/^ {12}/gm, '').trim()
+            value: '1'
         }   
     }
 }
