f165c77ae62980d6a585d74744a6c11fe44fdd76
lovexwu <1062474646@qq.com>
2019-04-23 13:48:35 +0800

    更新 toast提示框

--- docs/.vuepress/components/toast-demo-1.vue
+++ docs/.vuepress/components/toast-demo-1.vue
@@ -1,32 +1,15 @@
 <template>
-    <div style="padding-top: 16px;">
-        <h2>this.$toast</h2>
-        <p>
-            <strong>预览</strong>
-        </p>
-
+    <div style="padding-top: 15px;">
         <div>
             <g-button @click="$toast('顶部弹出提示')">顶部弹出</g-button>
             <g-button @click="$toast('中间弹出提示', {position:'middle'})">中间弹出</g-button>
             <g-button @click="$toast('底部弹出提示', {position:'bottom'})">底部弹出</g-button>
         </div>
-
-        <p>
-            <strong>代码</strong>
-        </p>
-        <pre><code>{{content}}</code></pre>
     </div>
 </template>
 
 <style>
-    .toast-wrapper {
-        z-index: 30 !important;
-    }
-</style>
-<style scoped>
-    * {
-        box-sizing: border-box;
-    }
+    .toast-wrapper {z-index: 30 !important;}
 </style>
 
 <script>
@@ -36,20 +19,6 @@
     Vue.use(plugin)
 
     export default {
-        components: {'g-button': GButton},
-        data () {
-           return {
-           content: `
-                <style>
-                .toast-wrapper {
-                  z-index: 30;
-                }
-                </style>
-                <g-button @click="$toast('顶部弹出提示')">顶部弹出</g-button>
-                <g-button @click="$toast('中间弹出提示', {position:'middle'})">中间弹出</g-button>
-                <g-button @click="$toast('底部弹出提示', {position:'bottom'})">底部弹出</g-button>
-           `.replace(/^ {12}/gm, '').trim()
-           }
-        }
+        components: {'g-button': GButton}
     }
 </script>
--- docs/.vuepress/components/toast-demo-2.vue
+++ docs/.vuepress/components/toast-demo-2.vue
@@ -1,26 +1,10 @@
 <template>
-    <div style="padding-top: 16px;">
-        <h2>带关闭按钮</h2>
-        <p>
-            <strong>预览</strong>
-        </p>
-
+    <div style="padding-top: 15px;">
         <div>
             <g-button @click="onClickButton">顶部弹出</g-button>
         </div>
-
-        <p>
-            <strong>代码</strong>
-        </p>
-        <pre><code>{{content}}</code></pre>
     </div>
 </template>
-
-<style scoped>
-    * {
-        box-sizing: border-box;
-    }
-</style>
 
 <script>
     import plugin from '../../../src/plugin'
@@ -30,33 +14,9 @@
 
     export default {
         components: {'g-button': GButton},
-        data () {
-            return {
-            content: `
-                <style>
-                .toast-wrapper {
-                  z-index: 30;
-                }
-                </style>
-                 <div>
-                    <g-button @click="onClickButton">顶部弹出</g-button>
-                 </div>
-                methods: {
-                    onClickButton () {
-                        this.$toast('顶部弹出内容', {
-                            closeButton: {
-                                text: '关闭'
-                            },
-                            enableHtml: true
-                        })
-                    }
-                }
-            `.replace(/^ {12}/gm, '').trim()
-                }
-            },
         methods: {
             onClickButton () {
-                this.$toast('顶部弹出内容', {
+                this.$toast('<strong style="color:red;">我是加粗的红色顶部弹出内容</strong>', {
                     closeButton: {
                         text: '关闭'
                     },
