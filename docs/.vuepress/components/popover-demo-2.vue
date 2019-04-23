46451593424a7c670829458596e0925c9fcdc3d4
lovexwu <1062474646@qq.com>
2019-04-23 13:47:42 +0800

    更新 popover 弹出框

--- docs/.vuepress/components/popover-demo-2.vue
+++ docs/.vuepress/components/popover-demo-2.vue
@@ -1,40 +1,23 @@
 <template>
-  <div> 
-   <h2>hover弹出</h2>
-   <p> <strong>预览</strong> </p> 
-   <g-popover trigger="hover"> 
-    <template slot="content"> 
-     <div>popover内容</div> 
-    </template> 
-    <g-button>Top</g-button> 
-   </g-popover> 
-   <g-popover position="bottom" trigger="hover"> 
-    <template slot="content"> 
-     <div>popover内容</div> 
-    </template> 
-    <g-button>Bottom</g-button> 
-   </g-popover> 
-   <g-popover position="left" trigger="hover">
-    <template slot="content"> 
-     <div>popover内容</div> 
-    </template> 
-    <g-button>Left</g-button> 
-   </g-popover> 
-   <g-popover position="right" trigger="hover">
-    <template slot="content"> 
-     <div>popover内容</div> 
-    </template> 
-    <g-button>Right</g-button> 
-   </g-popover> 
-   <p> <strong>代码</strong> </p> 
-   <pre><code>{{content}}</code></pre> 
-  </div>
+ <div style="padding-top: 15px;">
+     <g-popover trigger="hover">
+         <template slot="content"><div>popover内容</div></template>
+         <g-button>上方弹出</g-button>
+     </g-popover>
+     <g-popover position="bottom" trigger="hover">
+         <template slot="content"><div>popover内容</div></template>
+         <g-button>下方弹出</g-button>
+     </g-popover>
+     <g-popover position="left" trigger="hover">
+         <template slot="content"><div>popover内容</div></template>
+         <g-button>左边弹出</g-button>
+     </g-popover>
+     <g-popover position="right" trigger="hover">
+         <template slot="content"><div>popover内容</div></template>
+         <g-button>右边弹出</g-button>
+     </g-popover>
+ </div>
 </template>
-
-<style scoped>
-    * {box-sizing: border-box;}
-    h2{margin: 50px 0 30px;}
-</style>
 
 <script>
     import Popover from '../../../src/popover'
@@ -44,41 +27,7 @@
      components: {
          'g-button': Button,
          'g-popover': Popover
-     },
-     data(){
-         return{
-         content:`
-             <g-popover trigger="hover">
-                 <template slot="content">
-                     <div>popover内容</div>
-                 </template>
-                 <g-button>Top</g-button>
-             </g-popover>
-
-             <g-popover position="bottom" trigger="hover">
-                 <template slot="content">
-                     <div>popover内容</div>
-                 </template>
-                 <g-button>Bottom</g-button>
-             </g-popover>
-
-             <g-popover position="left" trigger="hover">
-                 <template slot="content">
-                     <div>popover内容</div>
-                 </template>
-                 <g-button>Left</g-button>
-             </g-popover>
-
-             <g-popover position="right" trigger="hover">
-                 <template slot="content">
-                     <div>popover内容</div>
-                 </template>
-                 <g-button>Right</g-button>
-             </g-popover>
-
-         `.replace(/^ {12}/gm, '').trim()
-         }
-   }
+     }
  }
 </script>
 
